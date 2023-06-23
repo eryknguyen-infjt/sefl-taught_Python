7a8d0df21233ae236f0aced6b682239a2ffe5b95
vy <stevenguyen2011dark@gmail.com>
2023-06-23 20:40:56 +0700

    Initial commit

--- freecodecamp_course.py
+++ freecodecamp_course.py
@@ -2,13 +2,13 @@
 
 # 1.So there is a basic code:
 nam = input('Who are you? ')
-print('Welcome to my world,', nam)
+print('Welcome to my world, ',nam)
 
-x = input('Enter your name : ')
+x = input('Enter your name: ')
 print('Hello ',x)
 
 # 2.There is a more difficult than first one:
 hrs = input('Enter Hours: ')
 r = input('Enter Rate: ')
 z = float(hrs) * float(r)
-print("Pay: ",z)
+print('Pay: ',z)
--- py.py
+++ py.py
@@ -1,17 +1,3 @@
-       # User Input in Freecodecamp course
-
-# 1.So there is a basic code:
-nam = input('Who are you? ')
-print('Welcome to my world,', nam)
-
-x = input('Enter your name : ')
-print('Hello ',x)
-
-# 2.There is a more difficult than first one:
-hrs = input('Enter Hours: ')
-r = input('Enter Rate: ')
-z = float(hrs) * float(r)
-print("Pay: ",z)
 
 
 
