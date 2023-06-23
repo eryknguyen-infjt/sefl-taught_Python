9dd59bab498972ec3b05e51db737860677a15629
vy <stevenguyen2011dark@gmail.com>
2023-06-23 21:42:15 +0700

    Initial commit

---
+++ freecodecamp_course.py
@@ -0,0 +1,12 @@
+# 1.So there is a basic code:
+nam = input('Who are you? ')
+print('Welcome to my world,', nam)
+
+x = input('Enter your name: ')
+print('Hello', x)
+
+# 2.There is a more difficult than first one:
+hrs = input('Enter Hours: ')
+r = input('Enter Rate: ')
+z = float(hrs) * float(r)
+print('Pay:', z)
--- py.py
+++ py.py
@@ -1,37 +1,3 @@
-# def statement (this statement don't have argument)
-def x_name():
-    return 'Alex'
-
-x = x_name()
-print(x)
-
-# def statement (this statement has argument)
-def a_forest(argument):
-    another_tree = argument * 1
-    return another_tree
-
-total = a_forest(6)
-print(total)
-
-# This is nesting (or nested functions)
-#folding sth
-def ball_in_box(argument):
-    def ball_out_box():
-        return 6
-    ball = ball_out_box()
-    special = argument / ball
-    return special
-
-final = ball_in_box(60)
-print(final)
-
-# The way we can write comments in Python
-'''
-unvailable
-belief
-anyway
-This way we can write comments in Python with more than 1 line
-'''
 
 
 
