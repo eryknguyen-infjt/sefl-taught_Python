70b4368c62de33f1faf673beb113ff2b803d479b
vy <stevenguyen2011dark@gmail.com>
2023-06-24 15:29:47 +0700

    Initial commit

--- py.py
+++ py.py
@@ -1,6 +1,31 @@
+# def statement
 
+# def statement without argumnent
+def pyn_name():
+    return 'Alex'
 
+x = pyn_name()
+print(x)
 
+# def statement with argument
+def a_forest(argument):
+    another_tree = argument * 1
+    return another_tree
+
+z = a_forest(6)
+print(z)
+
+# nesting (or nested statement)
+# folding sth
+def ball_in_box(argument):
+    def ball_out_box():
+        return 2
+    x = ball_out_box()
+    alone = argument / x
+    return alone
+
+final = ball_in_box(6)
+print(final)
 
 
 
