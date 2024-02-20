c29f003337c3626b8a719fd33873c9a97ab9c8b4
vy <stevenguyen2011dark@gmail.com>
2024-02-20 21:15:42 +0700

    Initial commit

--- py.py
+++ py.py
@@ -1,93 +1,70 @@
-import csv
-class DictionaryAPIen:
-    def __init__(self, csv_han):
-        self.api_en = csv_han
-
-# Next steps: we'll use CRUD(create, read, update, delete) -> C = lookup, R = add, U = update, D = delete
-    def lookup(self, word, definition): # C
-        with open(self.api_en, mode='r') as file:
-            reader = csv.DictReader(file)
-            for k in reader:
-                if k[word] == word:
-                    return k[definition]
-        return f" '{word}' not found in the dictionary."
-
-    def add(self, word, definition):
-        with open(self.api_en, mode='a') as file:
-            writer = csv.DictWriter(file, fieldnames=['word', 'definition'])
-            writer.writerow({'word': word, 'definition': definition})
-
-    def update(self, word, n_definition):
-        data = dict()
-        with open(self.api_en, mode='r') as file:
-            reader = csv.DictReader(file)
-            for k in reader:
-                if k[word] == word:
-                    k['definition'] = n_definition
-                data.append(k)
-
-        with open(self.api_en, mode='r') as file:
-            writer = csv.DictWriter(file, fieldnames=['word', 'definition'])
-            writer.writeheader()
-            writer.writerows(data)
-
-    def delete(self, word):
-        content = dict()
-        with open(self.api_en, mode='r') as file:
-            reader = csv.DictReader(file)
-            for k in reader:
-                if k[word] != word:
-                    content.append(k)
-
-        with open(self.api_en, mode='w') as file:
-            writer = csv.DictWriter(file, fieldnames=['word', 'definition'])
-            writer.writeheader()
-            writer.writerows(content)
-
-if __name__ == '__main__':
-    dictionary = DictionaryAPIen('dict.csv')
-
-    while True:
-        print("\nDictionary Menu (choice):")
-        print("1. Lookup word.")
-        print("2. Add word.")
-        print("3. Update definition.")
-        print("4. Delete word.")
-        print("5. Exit.")
-
-        choice = input('Enter your choice: ')
-
-        if choice == '1':
-            word = input('Enter any word: ')
-            definition = dictionary.lookup(word)
-            print(f"{word}: {definition}")
-
-        elif choice == '2':
-            word = input('Enter a new word: ')
-            definition = input("Enter the definition: ")
-            dictionary.add(word,definition)
-            print(f"'{word}' has been added to the dictionary.")
-
-        elif choice == '3':
-            word = input('Enter any word: ')
-            new_definition = input('Enter the new definiton: ')
-            dictionary.update(word, new_definition)
-            print(f"'{word}' has been updated.")
-
-        elif choice == '4':
-            word = input('Enter any word: ')
-            dictionary.delete(word)
-            print(f"'{word}' has been deleted from the dictionary.")
-
-        elif choice == '5':
-            break
-
-        else:
-            print("Invalid choice. Please enter a valid option.")
+# blackjack project with python
+import random
 
 
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
 
 
+class Deck:  # Deck
+    def __init__(self):
+        self.cards = []  # Every variable that want to use again need be in structor: self.(name's variable)
+        suits = ['spades', 'clubs', 'hearts', 'diamonds']
+        ranks = [{'rank': 'A', 'value': 11},
+                 {'rank': '2', 'value': 2},
+                 {'rank': '3', 'value': 3},
+                 {'rank': '4', 'value': 4},
+                 {'rank': '5', 'value': 5},
+                 {'rank': '6', 'value': 6},
+                 {'rank': '7', 'value': 7},
+                 {'rank': '8', 'value': 8},
+                 {'rank': '9', 'value': 9},
+                 {'rank': '10', 'value': 10},
+                 {'rank': 'J', 'value': 10},
+                 {'rank': 'Q', 'value': 10},
+                 {'rank': 'K', 'value': 10}]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt
 
 
+class Hand:
+    def __init__(self, dealer=False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
 
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+    def calculate_value(self):
+        self.value = 0
+
+        for card in self.cards:
+            card_value = int(card.rank['value'])
+            self.value += card_value
+
+
+deck = Deck()
+deck.shuffle()
+
+hand = Hand()
+hand.add_card(deck.deal(2))
+print(hand.cards[1], hand.cards[0])
