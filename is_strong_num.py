effe54f57666082e32cb3f123b3415e36a093e4e
眼 万 年 <135793580+eryknguyen-infjt@users.noreply.github.com>
2024-03-10 21:56:27 +0700

    if strong numbers

--- is_strong_num.py
+++ is_strong_num.py
@@ -2,7 +2,7 @@ def is_strong(n):
     # Generate a list of prime numbers up to the square root of n
     primes = [2]
     i = 3
-    while i ** 2 <= n:
+    while i**2 <= n:
         is_prime = True
         for p in primes:
             if i % p == 0:
@@ -16,7 +16,7 @@ def is_strong(n):
 
     # Check if n is divisible by the square of any prime factor
     for p in primes:
-        if n % p == 0 and n % (p * p) != 0:
+        if n % p == 0 and n % p**2 != 0:
             return False
 
     return True
@@ -28,3 +28,25 @@ for n in range(1, 1001):
         strong_numbers.append(n)
 
 print(strong_numbers)
+
+# def prime_factors(n):
+#     factors = []
+#     i = 2
+#     while i * i <= n:
+#         if n % i:
+#             i += 1
+#         else:
+#             n //= i
+#             factors.append(i)
+#     if n > 1:
+#         factors.append(n)
+#     return factors
+#
+#
+# def is_strong(n):
+#     factors = prime_factors(n)
+#     return len(factors) <= len(set(factors))
+#
+#
+# strong_numbers = [n for n in range(1, 1001) if is_strong(n)]
+# print(strong_numbers)
