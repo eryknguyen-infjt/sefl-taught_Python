9ffe84eb4444c1b067df1963a0c13b465d6997b4
眼 万 年 <135793580+eryknguyen-infjt@users.noreply.github.com>
2024-03-17 22:00:47 +0700

    interest rate typicial

--- interest_rate.py
+++ interest_rate.py
@@ -1,11 +1,34 @@
-# first one that open 2 files to have the input and the output
+def read_input(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+    data = [list(map(int, line.split())) for line in lines]
+    return data
 
-x = ""
-x.split()
 
-c = int(x[0])  # số kỳ hạn
-t = int(x[1])  # time gửi
-a = int(x[3])  # origin money
-k = int(x[4])  # lãi có kỳ hạn
-h = int(x[5])  # lãi no kỳ hạn
+def calculate_amount(c, t, A, k, h, m):
+    if c == 0:
+        if t <= m:
+            return round(A * (1 + k / 100) ** t, 1)
+        else:
+            return round(A * (1 + k / 100) ** m + (A * (1 + h / 100) ** (t - m)), 1)
+    else:
+        if t <= c:
+            return round(A * (1 + k / 100) ** t, 1)
+        else:
+            return round(A * (1 + k / 100) ** c + (A * (1 + h / 100) ** (t - c)), 1)
 
+
+def write_output(file_name, amounts):
+    with open(file_name, 'w') as file:
+        for amount in amounts:
+            file.write(f'{amount}\n')
+
+
+def main():
+    input_data = read_input('general.inp')
+    output_data = [calculate_amount(*data) for data in input_data]
+    write_output('general.out', output_data)
+
+
+if __name__ == '__main__':
+    main()
