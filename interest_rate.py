cd50276a8cac96496991a84acb45ebcd052b6fde
眼 万 年 <135793580+eryknguyen-infjt@users.noreply.github.com>
2024-03-19 21:21:46 +0700

    interest rate commit

--- interest_rate.py
+++ interest_rate.py
@@ -1,34 +1,29 @@
-def read_input(file_name):
-    with open(file_name, 'r') as file:
-        lines = file.readlines()
-    data = [list(map(int, line.split())) for line in lines]
-    return data
+file = open('general.inp', 'r+')
+findout = open('general.out', 'w')
 
+lines = file.readline()
+lines = lines.split()
 
-def calculate_amount(c, t, A, k, h, m):
+c = int(file[0])
+t = int(file[1])
+a = int(file[2])
+k = int(file[3])
+h = int(file[4])
+
+def calculate_amount(c, t, a, k, h, m):
     if c == 0:
         if t <= m:
-            return round(A * (1 + k / 100) ** t, 1)
+            return round(a * (1 + k / 100) ** t, 1)
         else:
-            return round(A * (1 + k / 100) ** m + (A * (1 + h / 100) ** (t - m)), 1)
+            return round(a * (1 + k / 100) ** m + (a * (1 + h / 100) ** (t - m)), 1)
     else:
         if t <= c:
-            return round(A * (1 + k / 100) ** t, 1)
+            return round(a * (1 + k / 100) ** t, 1)
         else:
-            return round(A * (1 + k / 100) ** c + (A * (1 + h / 100) ** (t - c)), 1)
+            return round(a * (1 + k / 100) ** c + (a * (1 + h / 100) ** (t - c)), 1)
 
 
-def write_output(file_name, amounts):
-    with open(file_name, 'w') as file:
-        for amount in amounts:
-            file.write(f'{amount}\n')
+findout.write(str(m))
 
-
-def main():
-    input_data = read_input('general.inp')
-    output_data = [calculate_amount(*data) for data in input_data]
-    write_output('general.out', output_data)
-
-
-if __name__ == '__main__':
-    main()
+file.close()
+findout.close()
